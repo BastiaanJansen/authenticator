9029c92514a31e02c32bcefe7dcb3155eb1ff34d
Bastiaan Jansen <Bastiaanj7@outlook.com>
2020-12-06 16:18:41 +0100

    QR code scanner

--- Authenticator/ViewModels/ScanQRCodeViewModel.swift
+++ Authenticator/ViewModels/ScanQRCodeViewModel.swift
@@ -6,7 +6,50 @@
 //
 
 import Foundation
+import CoreData
+import Combine
 
 class ScanQRCodeViewModel: ObservableObject {
+    var context: NSManagedObjectContext?
+    var viewDismissalModePublisher = PassthroughSubject<Bool, Never>()
+    private var shouldDismissView = false {
+        didSet {
+            viewDismissalModePublisher.send(shouldDismissView)
+        }
+    }
+    
     @Published var showAddAccountView: Bool = false
+    
+    func foundBarcode(value: String) {
+        var map: [String: String] = [:]
+        let info = value.split(separator: "/")[2]
+        
+        let paramInfo = info.split(separator: "?")
+        let name = paramInfo[0].split(separator: ":")[1]
+        map["name"] = String(name)
+        
+        let params = paramInfo[1].split(separator: "&")
+        
+        params.forEach { param in
+            let pair = param.split(separator: "=")
+            map[String(pair[0])] = String(pair[1])
+        }
+        
+        let _ = self.createAccount(service: map["issuer"]!, name: map["name"]!, key: map["secret"]!)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            print("sds")
+            self.shouldDismissView = true
+        }
+        
+    }
+    
+    func createAccount(service: String, name: String, key: String) -> Account {
+        guard let context = self.context else { fatalError("Context is not set") }
+        let account = Account(context: context, service: service, name: name, key: key)
+        
+        context.saveContext()
+        
+        return account
+    }
 }
--- Authenticator/Views/ScanQRCodeView.swift
+++ Authenticator/Views/ScanQRCodeView.swift
@@ -6,25 +6,76 @@
 //
 
 import SwiftUI
+import CarBode
+import AVFoundation
 
 struct ScanQRCodeView: View {
     @Environment(\.presentationMode) var presentationMode
+    @Environment(\.managedObjectContext) private var viewContext
+    
     @ObservedObject var scanQRCodeVM = ScanQRCodeViewModel()
     
     var body: some View {
-        NavigationView {
-            VStack {
-                Spacer()
-                Button(action: {
-                    self.scanQRCodeVM.showAddAccountView.toggle()
-                }) {
-                    Text("Enter code manually")
-                }.sheet(isPresented: self.$scanQRCodeVM.showAddAccountView, onDismiss: {
-                    self.presentationMode.wrappedValue.dismiss()
-                }) {
-                    AddAccountView()
+        ZStack {
+            GeometryReader { geo in
+                CBScanner(
+                    supportBarcode: .constant([.qr]),
+                    scanInterval: .constant(2.0)
+                ){
+                    self.scanQRCodeVM.foundBarcode(value: $0.value)
                 }
-            }.navigationTitle("Scan QR Code")
+
+                VStack {
+                    Spacer()
+                    
+                    HStack {
+                        Button(action: {
+                            self.presentationMode.wrappedValue.dismiss()
+                        }) {
+                            Text("Cancel")
+                                .foregroundColor(.black)
+                                .font(.system(size: 14))
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(
+                                    Rectangle()
+                                        .foregroundColor(.white)
+                                        .cornerRadius(10)
+                                        .frame(width: geo.size.width / 2 - 30))
+                        }
+                        
+                        Button(action: {
+                            self.scanQRCodeVM.showAddAccountView.toggle()
+                        }) {
+                            Text("Enter code manually")
+                                .foregroundColor(.black)
+                                .font(.system(size: 14))
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(
+                                    Rectangle()
+                                        .foregroundColor(.white)
+                                        .cornerRadius(10)
+                                        .frame(width: geo.size.width / 2 - 30))
+                        }
+                        .sheet(isPresented: self.$scanQRCodeVM.showAddAccountView, onDismiss: {
+                            self.presentationMode.wrappedValue.dismiss()
+                        }) {
+                            AddAccountView()
+                        }
+                    }
+                }.padding(.bottom, 50)
+            }
+        }
+        .ignoresSafeArea(.all, edges: .all)
+        .onAppear() {
+            self.scanQRCodeVM.context = viewContext
+        }
+        .onReceive(scanQRCodeVM.viewDismissalModePublisher) { shouldDismiss in
+            print(shouldDismiss)
+            if shouldDismiss {
+                self.presentationMode.wrappedValue.dismiss()
+            }
         }
     }
 }
